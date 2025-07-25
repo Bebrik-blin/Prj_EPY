ae8ade0fc13d139202aed9d623fe4956f8a3c941
GoneThisBlastwave <lovehueta@gmail.com>
2025-07-25 10:07:31 +0300

    Update geometry.py
    Закоментировано, немного допилено

--- geometry.py
+++ geometry.py
@@ -1,6 +1,7 @@
 #
 #Оставь надежду, всяк сюда входящий. Я сам не знаю как в этом работать
 
+#Создаём класс точки, у неё 3 параметра x, y, z
 class point:
 
 	def __init__(self, x, y, z):
@@ -8,7 +9,8 @@ class point:
 		self.y = y
 		self.z = z
 	
-	def sum_point_to_point(self, p):
+	#Сумма\разность точек даёт вектор
+	def add_point_to_point(self, p):
 		sx = self.x+p.x
 		sy = self.y+p.y
 		sz = self.z+p.z
@@ -16,7 +18,7 @@ class point:
 
 		return(nvector)
 
-	def dev_point_from_point(self, p):
+	def sub_point_from_point(self, p):
 		sx = self.x-p.x
 		sy = self.y-p.y
 		sz = self.z-p.z
@@ -24,7 +26,8 @@ class point:
 
 		return(nvector)
 	
-	def sum_vector_to_point(self, v):
+	#Сумма\разность точки и ветора даёт точку
+	def add_vector_to_point(self, v):
 		sx = self.x+v.xf
 		sy = self.y+v.yf
 		sz = self.z+v.zf
@@ -32,7 +35,7 @@ class point:
 		
 		return(npoint)
 	
-	def dev_vector_from_point(self, v):
+	def sub_vector_from_point(self, v):
 		sx = self.x-v.xf
 		sy = self.y-v.yf
 		sz = self.z-v.zf
@@ -40,9 +43,11 @@ class point:
 	
 		return(npoint)
 
+	#Функция возврата
 	def draw(self):
 		return(self.x, self.y, self.z)
 
+#Класс вектора, принимает x, y, z
 class vector:
 
 	def __init__(self, xf, yf, zf):
@@ -50,19 +55,49 @@ class vector:
 		self.yf = yf
 		self.zf = zf
 	
-	def sum_vector_to_vector(self, v):
+	#Сумма/разность векторов даёт вектор
+	def add_vector_to_vector(self, v):
 		sxf = self.xf+v.xf
 		syf = self.yf+v.yf
 		szf = self.zf+v.zf
 		svector = vector(sxf, syf, szf)
 		return(svector)
 	
-	def dev_vector_from_vector(self, v):
+	def sub_vector_from_vector(self, v):
 		sxf = self.xf-v.xf
 		syf = self.yf-v.yf
 		szf = self.zf-v.zf
 		svector = vector(sxf, syf, szf)
 		return(svector)
 
+	#Функция возврата
 	def draw(self):
 		return(self.xf, self.yf, self.zf)
+
+#Класс 2д вектора, принимает x, y
+class vector_plain:
+
+	def __init__(self, xp, yp):
+		self.x = xp
+		self.y = yp
+
+	#Функция возврата
+	def draw(self):
+	 	return(self.x, self.y)
+
+#Класс мат.функций, не имеет переменных которые надо создать в конструкторе, пока нет
+class math:
+
+	def __init__():
+		#Опа, а тут нихуя нет
+		pass
+
+	#Функция перевода вектора из 3д в 2д(рабочая)
+	def vector_to_plain(v):
+
+		xp = v.yf*v.zf
+		yp = v.xf*v.zf*(-1)
+		n = vector_plain(xp, yp)
+		return(n)
+	
+	#Допилим другие ф-ции по мере надобности
